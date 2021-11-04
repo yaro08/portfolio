3d2f21d8cb706eac57b1668209a0a27483ff392c
YAROSLAV TRUSH <yaroslav.trush@goumh.umh.es>
2021-11-04 22:50:37 +0100

    updated server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+app.use(express.static(__dirname+'/dist/cryptocurrency'));
+app.get('/*',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/cryptocurrency/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
